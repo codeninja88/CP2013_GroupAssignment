cc788d6c99c77fb1e0b558cebbaf3f832131b15f
jc299305 <nicholas.gregory@my.jcu.edu.au>
2015-09-25 14:05:02 +1000

    made it super mobile friendly

--- mainApp/expandMenu.js
+++ mainApp/expandMenu.js
@@ -2,18 +2,19 @@ function expandMenu() {
     var click = document.getElementById("dropDownMenu");
     var menu = document.getElementById("menu");
     var width = screen.width;
-
+    console.log(click.value);
+    console.log(width);
     if ( width < 768) {
 
         if (click.value === "show") {
             click.value = "hide";
             menu.style.display = "none";
-            console.log("SUP NIGGA 1");
+            console.log("SUP");
 
         } else {
             click.value = "show";
             menu.style.display = "block";
-            console.log("SUP NIGGA 2");
+            console.log("SUP 2");
         }
     } else {
         click.value = "show";
